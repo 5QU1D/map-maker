e66711a33948f49112cb4269ca699266f36d38c8
5QU1D <sydainsworth@gmail.com>
2025-02-25 13:24:11 -0800

    working through logic of reading into 3d array

--- js/testRead.js
+++ js/testRead.js
@@ -14,6 +14,52 @@ document
             // let tile : [char, float]; //only a valid structure in TS
             // a guide to TS arrays and tuples: https://type-level-typescript.com/arrays-and-tuples
 
+            //from James
+            // let tupleArray: [string, number][][] = [
+            //     [
+            //       ["apple", 1],
+            //       ["banana", 2]
+            //     ],
+            //     [
+            //       ["cherry", 3],
+            //       ["date", 4]
+            //     ],
+            //     [
+            //       ["elderberry", 5],
+            //       ["fig", 6]
+            //     ]
+            //   ];
+
+            // same sort of structure, I just can't do type annotations
+            // let threeDArray = [
+            //     [
+            //       ['A1', 'A2'],
+            //       ['B1', 'B2']
+            //     ],
+            //     [
+            //       ['C1', 'C2'],
+            //       ['D1', 'D2']
+            //     ],
+            //     [
+            //       ['E1', 'E2'],
+            //       ['F1', 'F2']
+            //     ]
+            //   ];
+
+            // building 3d arrays : https://stackoverflow.com/questions/4943633/creating-and-parsing-a-3d-array-in-javascript
+
+            // var myArr = new Array();
+            // myArr[0] = new Array();
+            // myArr[0][0] = new Array();
+            // myArr[0][0][0] = "Howdy";
+            // myArr[0][0][1] = "pardner";
+
+            // for (var i = 0; i<numberOfQuestions; i++){
+            //     data[i] = new Array();
+            //     data[i][0] = something;
+            //     data[i][1] = somethingElse;
+            //    }
+
             // read from array (built from buffer) into bytes (empty array we declared on line 13)
             for (let i = 0; i < Math.min(20, fileSize); i++) {
                 bytes.push(array[i]);
