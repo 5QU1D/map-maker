c6f3181f15f3a9c788a38c8cb1798cb12269e13c
5QU1D <sydainsworth@gmail.com>
2025-03-10 12:17:15 -0700

    shifted file read into it's own function; called read and draw in onload()

--- js/testDraw.js
+++ js/testDraw.js
@@ -2,8 +2,10 @@
 // so that this fires AFTER reading the file
 // and loading its contents
 
+// settimeout does not seem to be a viable solution to this, even as a placeholder. sadge.
+
 document
     .getElementById('FileInput')
-    .addEventListener('change', function (event) {
+    .addEventListener('change', function () {
         drawGrid(tilesWidth,tilesHeight);
     });
