351324df9f49106f455e426f50625374165b2dd9
Sydney Ainsworth <sydainsworth@gmail.com>
2025-05-11 23:44:12 -0700

    corrected load.js to match variants branch

--- js/load.js
+++ js/load.js
@@ -8,8 +8,23 @@ document
             document.getElementById('finMap').src='';
             read(event);
             document.getElementById('loader').style.display='block';
+
+            //check content of radio button to determine function call for drawGrid
             radioVal = getSelectedRadioValue("tileset");
-            drawGrid(tilesWidth,tilesHeight);
+
+            switch(radioVal) {
+                case 'fullsketch':
+                case 'noext':
+                    drawGrid(tilesWidth,tilesHeight);
+                    break;
+                case 'colortiles':
+                    var numTilesW = tilesWidth/300;
+                    var numTilesH = tilesHeight/300;
+                    drawcolGrid(200*numTilesW,200*numTilesH);
+                    break;
+            }
+            //all other adjustments for function calls are in tileParse.js
+
             blobber();
         };
 
