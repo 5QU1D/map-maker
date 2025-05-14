aea40dc4d61ba7e940dca332124e279ebdaf7f76
Sydney Ainsworth <sydainsworth@gmail.com>
2025-05-13 22:42:30 -0700

    removed superflous if, added comments, removed old code

--- js/load.js
+++ js/load.js
@@ -4,31 +4,6 @@ document
         let file = event.target.files[0];
         let reader = new FileReader();
 
-        // reader.onload = function (event) {
-        //     document.getElementById('finMap').src='';
-        //     read(event);
-        //     document.getElementById('loader').style.display='block';
-
-        //     //check content of radio button to determine function call for drawGrid
-        //     radioVal = getSelectedRadioValue("tileset");
-
-        //     switch(radioVal) {
-        //         case 'fullsketch':
-        //         case 'noext':
-        //             drawGrid(tilesWidth,tilesHeight);
-        //             break;
-        //         case 'colortiles':
-        //             var numTilesW = tilesWidth/300;
-        //             var numTilesH = tilesHeight/300;
-        //             drawcolGrid(200*numTilesW,200*numTilesH);
-        //             break;
-        //     }
-        //     //all other adjustments for function calls are in tileParse.js
-
-        //     blobber();
-        //     document.getElementById("FileInput").value = ''; //reset the input file
-        // };
-
         reader.onload = function (event) {
             read(event);
         }
